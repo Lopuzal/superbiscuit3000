dd13d8f6192cceea26721d45b43e6f2e80d3cfdd
Lopuzal <fuzo3000@outlook.fr>
2025-08-27 16:04:34 +0200

    added player and merged with ennemy branch

---
+++ player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var speed: float = 400.0
+
+func get_input() -> void:
+	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * speed
+	
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_collide(velocity * delta)
