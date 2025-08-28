733ee6933430d0ed38ed500b8ad48ee05d36684e
Lopuzal <fuzo3000@outlook.fr>
2025-08-28 23:10:52 +0200

    added sprites for attack

--- player.gd
+++ player.gd
@@ -2,11 +2,17 @@ extends CharacterBody2D
 
 @export var speed: float = 400.0
 
+
+
 func get_input() -> void:
 	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	look_at(get_global_mouse_position())
 	velocity = input_direction * speed
 	
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("attack"):
+		$AnimatedSprite2D.play("default")
+
 func _physics_process(delta: float) -> void:
 	get_input()
 	move_and_collide(velocity * delta)
