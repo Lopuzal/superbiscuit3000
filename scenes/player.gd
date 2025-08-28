f5a015fff1d8332838d65c37fa11872700edb667
Meh Estout <truefate@mailo.com>
2025-08-28 22:06:49 +0200

    Added hitbox class and enemy spawners, enemies can now deal damage to player

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,5 +1,11 @@
 extends CharacterBody2D
 
+@export var hit_points: float = 100.0 :
+	set(value):
+		hit_points = value
+		if hit_points <= 0 :
+			die()
+		
 @export var speed: float = 400.0
 
 func get_input() -> void:
@@ -10,3 +16,11 @@ func get_input() -> void:
 func _physics_process(delta: float) -> void:
 	get_input()
 	move_and_collide(velocity * delta)
+
+func die() -> void:
+	print("player dead lol")
+	queue_free()
+
+func _on_hit_box_received_damage(damage_point: float) -> void:
+	hit_points -= damage_point
+	print("player received damage, new hit points = ", hit_points)
