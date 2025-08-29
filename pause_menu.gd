2ceb82d223826df2ff6d21e685807f0c2f95da87
unknown <roseviolette.marly@gmail.com>
2025-08-29 12:05:20 +0200

    retry and music background

--- pause_menu.gd
+++ pause_menu.gd
@@ -2,7 +2,7 @@ extends ColorRect
 
 
 func _on_continue_pressed() -> void:
-	get_tree().paused = not get_tree().paused
+	get_tree().paused = false
 	hide()
 
 
@@ -13,3 +13,17 @@ func _on_exit_pressed() -> void:
 func _on_visibility_changed() -> void:
 	if visible:
 		$VBoxContainer/Continue.grab_focus()
+
+
+func _on_retry_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+
+func _on_toggle_music_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		NodeReferences.Music.play()
+		print("Music is on!")
+	else:
+		NodeReferences.Music.stop()
+		print("Music is off!")
